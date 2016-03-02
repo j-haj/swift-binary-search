71be78316790b1e302026ffaa8c36719c3591ea8
Jeff Hajewski <jeff.hajewski@gmail.com>
2016-03-02 08:40:58 -0500

    Moves implementation from Array to CollectionType

--- BinarySearch.playground/Contents.swift
+++ BinarySearch.playground/Contents.swift
@@ -5,16 +5,28 @@
 // ======================================================================================================================
 
 extension CollectionType {
-	/// Returns an index such that each element at or above the indes is not less than the partition predicate
+	/// Returns an index such that each element at or above the index is partitioned from below by the partition predicate
 	///
-	/// - Parameter isOrderedBelow: The partioning predicate. Returns `true` for elements in the collection that are
-	///                             ordered below, with respet to the partitioning predicate.
+	/// - Parameter partitionPredicate: The partioning predicate returns `true` for elements in the collection that are
+	///                                 ordered below, with respet to the partitioning predicate.
 	/// - Complexity: O(lg(n))
 	///
-	/// - Returns: An index such that each element evaluates as `false` with respect to `isOrderedBelow(_:)`
+	/// - Returns: An index such that each element at or above the returned index evaluates as `false` with respect to `partitionPredicate(_:)`
 	@warn_unused_result
-    func lowerBound(@noescape isOrderedBelow: (partitioningValue: Self.Generator.Element) -> Bool) -> Index {
-		assertionFailure("Not implemented")
+    func lowerBound(@noescape partitionPredicate: Self.Generator.Element -> Bool) -> Index {
+        var len = self.startIndex.distanceTo(self.endIndex)
+        var firstIndex = self.startIndex
+        while len > 0 {
+            let half = len/2
+            let tmpIndex = firstIndex.advancedBy(half)
+            if partitionPredicate(self[tmpIndex]) {
+                firstIndex = tmpIndex.advancedBy(1)
+                len = len - half - 1
+            } else {
+                len = half
+            }
+        }
+        return firstIndex.advancedBy(-1)
 	}
 	
 	/// Returns an index such that each element below the index is strictly less than the partition predicate
@@ -25,10 +37,32 @@ extension CollectionType {
 	///
 	/// - Returns: An index such that each element evaluates as `false` with respect to `isOrderedBelow(_:)`
 	@warn_unused_result
-	func upperBound(@noescape isOrderedBelow: (partitioningValue: Self.Generator.Element) -> Bool) -> Index {
-		assertionFailure("Not implemented")
+	func upperBound(@noescape partitionPredicate:Self.Generator.Element -> Bool) -> Index {
+        var len = self.startIndex.distanceTo(self.endIndex)
+        var firstIndex = self.startIndex
+        while len > 0 {
+            let half = len/2
+            let tmpIndex = firstIndex.advancedBy(half)
+            if !partitionPredicate(self[tmpIndex]) {
+                len = half
+            } else {
+                firstIndex = tmpIndex.advancedBy(1)
+                len -= half + 1
+            }
+        }
+        return firstIndex
 	}
+    
+    @warn_unused_result
+    func binarySearch(@noescape partitionPredicate:Self.Generator.Element -> Bool) -> Bool {
+        let lb = lowerBound(partitionPredicate)
+        print("binary search lb: \(lb)")
+        return (lb != self.endIndex) && !partitionPredicate(self[lb])
+    }
+    
 }
+
+
 
 // ======================================================================================================================
 //
@@ -58,82 +92,6 @@ extension Array {
         // If executions reaches this point, collection must be sorted
         return true
     }
-	
-    /// Returns a collection such that each element in the collection is greater than or equal to `value`
-    ///
-    /// - Parameter value: The value that is less than or equal to all elements in the returned collection
-	/// - Parameter comparator: Returns `true` if `lhs` is less than `rhs`, returns `false` otehrwise
-	///
-    /// - Requires: The calling collection is expected to be sorted
-	///
-    /// - Complexity: O(lg(n))
-    ///
-    /// - Returns: The index of the element that provides a lower bound the the elements in the calling array
-    @warn_unused_result
-    func lowerBound(value: Array.Element, @noescape comparator: (lhs: Array.Element, rhs: Array.Element) -> Bool) -> Int {
-        // WARN: Should we check `isSorted` here?
-        precondition(self.isSorted(comparator), "The calling CollectionType must be sorted")
-        
-        var len = self.count
-        var firstIndex = 0
-        while len > 0 {
-            let half = len >> 1
-            let tmpIndex = firstIndex.advancedBy(half)
-            if comparator(lhs:self[tmpIndex], rhs:value) {
-                firstIndex = tmpIndex.advancedBy(1)
-                len = len - half - 1
-            } else {
-                len = half
-            }
-        }
-        return firstIndex
-    }
-    
-    /// Returns a collection such that each element in the collection is strictly less than `value`
-    ///
-    /// - Parameter value: The value that is strictly greater than all elements in the array
-	///
-    /// - Requires: The calling collection is expected to be sorted
-	///
-    /// - Complexity: O(lg(n))
-    ///
-    /// - Returns: An array whose elements are all strictly less than `value`
-    @warn_unused_result
-    func upperBound(value: Array.Element, @noescape comparator: (lhs: Array.Element, rhs: Array.Element) -> Bool) -> Int {
-		
-        var len = self.count
-        var firstIndex = 0
-        while len > 0 {
-            let half = len >> 1
-            let tmpIndex = firstIndex.advancedBy(half)
-            if comparator(lhs:value, rhs:self[tmpIndex]) {
-                len = half
-            } else {
-                firstIndex = tmpIndex.advancedBy(1)
-                len = len - half - 1
-            }
-        }
-        return firstIndex
-    }
-	
-	@warn_unused_result
-	func equalRange(value: Array.Element, @noescape comparator: (lhs: Array.Element, rhs: Array.Element) -> Bool) -> Range<Int>? {
-		assertionFailure("Not implemented yet")
-		return nil
-	}
-    
-    // ------------------------------------------------------------------------------------------------------------------
-    //
-    // MARK: Binary Search Signatures
-    //
-    // ------------------------------------------------------------------------------------------------------------------
-    
-    
-    @warn_unused_result
-    func binarySearch(value: Array.Element, @noescape comparator:(lhs: Array.Element, rhs: Array.Element) -> Bool) -> Bool {
-        let lb = lowerBound(value, comparator: comparator)
-        return lb != self.count && !comparator(lhs: value, rhs: self[lb])
-    }
     
 }
 
@@ -158,12 +116,23 @@ print(testDict.keys)
 print(testDict.keys.sort({ (lhs, rhs) -> Bool in
     return lhs < rhs
 }).isSorted{ $0 < $1 })
-
-let lower = sortedArray.lowerBound(3) { $0 < $1 }
-print("Lower is \(lower)")
 print("Sorted array is now: \(sortedArray)")
-let upper = sortedArray.upperBound(3) { $0 < $1 }
+
+let lower = sortedArray.lowerBound { $0 <= 3 }
+print("Lower is \(lower)")
+
+let upper = sortedArray.upperBound { $0 <= 3 }
 print("Upper is \(upper)")
 
-print("sortedArray contains 2: \(sortedArray.binarySearch(2){ $0 < $1 })")
-print("sortedArray contains 5: \(sortedArray.binarySearch(5){$0 < $1})")
+print("sortedArray contains 2: \(sortedArray.binarySearch{ $0 <= 2 })")
+print("sortedArray contains 5: \(sortedArray.binarySearch {$0 < 5})")
+
+let testArray1 = [4, 5, 9, 10, 19, 22, 24]
+let testLower1 = testArray1.lowerBound { $0 <= 9 }
+let testUpper1 = testArray1.upperBound { $0 <= 9 }
+print("For array: \(testArray1)\nLower bound for 9: \(testLower1)\nUpper bound for 9: \(testUpper1)")
+
+let testArray2 = [4, 5, 10, 19, 22, 24]
+let testLower2 = testArray1.lowerBound { $0 <= 9 }
+let testUpper2 = testArray1.upperBound { $0 <= 9 }
+print("For array: \(testArray2)\nLower bound for 9: \(testLower2)\nUpper bound for 9: \(testUpper2)")
